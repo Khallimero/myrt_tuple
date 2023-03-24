c6bdc31cc8c4c76d0be9e62c3b3951c525e4cc42
Kortex <kortex@z820>
2023-03-24 19:20:34 +0100

    Reverse LockQueue

--- system/LockQueue.h
+++ system/LockQueue.h
@@ -19,38 +19,43 @@ public:
 public:
     const Lockable* tryEnqueueLock()
     {
-        if(N==0) return lockable->trylock()==0?lockable:NULL;
-        for(int i=0; i<N; i++)
+        if(N==0)
         {
-            if(lckTab[i].trylock()==0)
+            if(lockable->trylock()==0)
             {
-                if(i>0)lckTab[i-1].unlock();
+                locked=true;
+                return lockable;
             }
-            else return i>0?&lckTab[i-1]:NULL;
+            return NULL;
         }
-        return &lckTab[N-1];
+        
+        for(int i=N; i>0; i--)
+            if(lckTab[i-1].trylock()==0)
+                return &lckTab[i-1];
+        return NULL;
     }
 
     void waitLock(const Lockable* l)
     {
         if(N>0)
         {
-            for(int i=0; i<N; i++)
+            for(int i=N; i>0; i--)
             {
-                if(&lckTab[i]==l)
+                if(&lckTab[i-1]==l)
                 {
-                    for(int j=i+1; j<N; j++)
+                    for(int j=i; j<N; j++)
                     {
                         lckTab[j].lock();
                         lckTab[j-1].unlock();
                     }
-                    lockable->lock();
+                    lock();
                     lckTab[N-1].unlock();
-                    break;
+                    return;
                 }
             }
         }
-        locked=true;
+        
+        lock();
     }
 
     void lock()
@@ -66,12 +71,6 @@ public:
             locked=false;
             lockable->unlock();
         }
-    }
-
-    void abort()
-    {
-        if(N==0)lockable->unlock();
-        else lckTab[0].unlock();
     }
 
 protected:
